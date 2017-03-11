27b346723ae65a237dcf633cd21a8bebbe6ae7b6
Shushan <noname320@hotmail.com>
2017-03-11 18:58:26 -0500

    Deleted contact

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { DetailTabsPage } from '../pages/detailTabs/detailTabs';
 import { DetailPage } from '../pages/detail/detail';
@@ -16,7 +15,6 @@ import { GoogleService } from "./services/google.service";
   declarations: [
     MyApp,
     AboutPage,
-    ContactPage,
     HomePage,
     DetailTabsPage,
     DetailPage,
@@ -30,7 +28,6 @@ import { GoogleService } from "./services/google.service";
   entryComponents: [
     MyApp,
     AboutPage,
-    ContactPage,
     HomePage,
     DetailTabsPage,
     DetailPage,
