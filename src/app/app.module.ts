47438a0c387342edeb12401d2e86de068830e4a4
Shushan <noname320@hotmail.com>
2017-03-11 19:48:46 -0500

    Revert "Deleted contact"
    This reverts commit 27b346723ae65a237dcf633cd21a8bebbe6ae7b6.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { AboutPage } from '../pages/about/about';
+import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { DetailTabsPage } from '../pages/detailTabs/detailTabs';
 import { DetailPage } from '../pages/detail/detail';
@@ -15,6 +16,7 @@ import { GoogleService } from "./services/google.service";
   declarations: [
     MyApp,
     AboutPage,
+    ContactPage,
     HomePage,
     DetailTabsPage,
     DetailPage,
@@ -28,6 +30,7 @@ import { GoogleService } from "./services/google.service";
   entryComponents: [
     MyApp,
     AboutPage,
+    ContactPage,
     HomePage,
     DetailTabsPage,
     DetailPage,
