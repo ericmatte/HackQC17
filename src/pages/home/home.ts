f5ff74b8971bc162ce59155c435aa1cf7c2473c1
icekev <klanthier@swiftcloudhosting.com>
2017-03-11 23:59:42 -0500

    EVERYTHING IS FINE NOW

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -4,11 +4,11 @@ import { DetailTabsPage } from '../detailTabs/detailTabs';
 import { DataService } from '../../app/services/data.service';
 import { WeatherService } from '../../app/services/weather.service';
 import { GoogleService, travelModes } from "../../app/services/google.service";
+import * as moment from "moment";
+import 'moment/locale/fr-ca';
 import 'rxjs/add/operator/map';
 import { Gauge, needle } from "../../assets/js/gauge";
 import * as $ from "jquery";
-import * as moment from "moment";
-import 'moment/locale/fr-ca';
 import * as q from "q";
 
 @Component({
@@ -31,7 +31,6 @@ export class HomePage {
       let _depart       = '1600 Boulevard du Plateau-Saint-Joseph, Sherbrooke, QC J1L 0C8';
       let _destination  = "3455 rue du Fer-Droit, Sherbrooke, QC J1H 0A8";
       let _date         = "2017-03-17T19:10:31.236Z";   
-
       this.calculatePercent(_depart,_destination,_date).then( (percentage) => {
           let type = this.getItemType(percentage);
           let icon = "walk";
@@ -64,6 +63,7 @@ export class HomePage {
             pourcentage : percentage,
             transport: transport
           });
+             
           this._ngZone.run(() => { });
       });
     }
@@ -73,17 +73,19 @@ export class HomePage {
 
   public calculatePercent(depart, destination, date) {
       var choise = [];
-        choise["velo"]    = 0;
-        choise["bus"]     = 0;
-        choise["marche"]  = 0;
-        choise["auto"]    = 0;
+        choise["velo"]    = {nb:0, time: 0, distance: 0};
+        choise["bus"]     = {nb:0, time: 0, distance: 0};
+        choise["marche"]  = {nb:0, time: 0, distance: 0};
+        choise["auto"]    = {nb:0, time: 0, distance: 0};
 
       return q.Promise((resolve, reject, notify) => {
           let i = 0;
-
           this.dataService.getAccidentRate("rate").then((accident) => {
-                choise[Object.keys(accident)[0]]+=2;  
-                choise[Object.keys(accident)[1]]+=1;       
+              choise[Object.keys(accident)[0]].nb +=2;  
+              choise[Object.keys(accident)[1]].nb +=1;   
+              
+              i++;  
+              if(i == 6)  resolve(choise);      
           });
 
           var oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
@@ -100,68 +102,83 @@ export class HomePage {
                 if(firstDate.getMonth() != 12 && firstDate.getMonth() != 1 && firstDate.getMonth() != 2) {
                   let temp = data.list[diffDays-1].temp.day - 273;
                   if(temp < -25) {
-                    choise["bus"] += 2; 
-                    choise["auto"] += 3; 
+                    choise["bus"].nb += 2; 
+                    choise["auto"].nb += 3; 
                   }
                   else if (temp < -10)  {
-                    choise["auto"] += 1; 
-                    choise["marche"] += 1; 
-                    choise["bus"] += 3;
+                    choise["auto"].nb += 1; 
+                    choise["marche"].nb += 1; 
+                    choise["bus"].nb += 3;
                   }
                   else if (temp < -10)  {
-                    choise["bus"] += 3;
-                    choise["marche"] += 2;
+                    choise["bus"].nb += 3;
+                    choise["marche"].nb += 2;
                   }
                   else {
-                    choise["bus"] += 3;
-                    choise["marche"] += 3;
+                    choise["bus"].nb += 3;
+                    choise["marche"].nb += 3;
                   }
-                } 
+                }
+                i++;  
+                if(i == 6)  resolve(choise); 
               },
               err => console.log('err: ' + err)
             );
           }
 
-          this.googleService.getGoogleEstimatedTime(travelModes.walking, "1600 Boulevard du Plateau-Saint-Joseph, Sherbrooke", "9 Rue du Cégep, Sherbrooke, QC J1E 2J4").then( (response) => {
-            if((response.duration.value/60/60) < 15)  choise["marche"] += 3;
-            else if((response.duration.value/60/60) < 30)  choise["marche"] += 2;
-            else if((response.duration.value/60/60) < 50)  choise["marche"] += 1;
+          this.googleService.getGoogleEstimatedTime(travelModes.walking, depart, destination).then( (response) => {
+            choise["marche"].time = response.duration.value;
+            choise["marche"].distance = response.distance.text;
+
+            if((response.duration.value/60/60) < 15)  choise["marche"].nb += 3;
+            else if((response.duration.value/60/60) < 30)  choise["marche"].nb += 2;
+            else if((response.duration.value/60/60) < 50)  choise["marche"].nb += 1;
            
             i++;  
-            if(i == 4)  resolve(choise);
+            if(i == 6)  resolve(choise);
           }).catch( (error) => {
             console.log(error);
           });
 
-          this.googleService.getGoogleEstimatedTime(travelModes.transit, "1600 Boulevard du Plateau-Saint-Joseph, Sherbrooke", "9 Rue du Cégep, Sherbrooke, QC J1E 2J4").then( (response) => {
-            if((response.duration.value/60/60) < 15)  choise["bus"] += 3;
-            else if((response.duration.value/60/60) < 30)  choise["bus"] += 2;
-            else if((response.duration.value/60/60) < 50)  choise["bus"] += 1;
-            
+          this.googleService.getGoogleEstimatedTime(travelModes.transit, depart, destination).then( (response) => {
+            if (!response && !response.status && response.status != "ZERO_RESULTS") {
+              
+              choise["bus"].time = response.duration.value;
+              choise["bus"].distance = response.distance.text;
+              if((response.duration.value/60/60) < 15)  choise["bus"].nb += 3;
+              else if((response.duration.value/60/60) < 30)  choise["bus"].nb += 2;
+              else if((response.duration.value/60/60) < 50)  choise["bus"].nb += 1;
+            }
             i++;  
-            if(i == 4)  resolve(choise);
+            if(i == 6)  resolve(choise);
           }).catch( (error) => {
             console.log(error);
           });
 
-          this.googleService.getGoogleEstimatedTime(travelModes.bicycling, "1600 Boulevard du Plateau-Saint-Joseph, Sherbrooke", "9 Rue du Cégep, Sherbrooke, QC J1E 2J4").then( (response) => {
-            if((response.duration.value/60/60) < 15)  choise["bus"] += 3;
-            else if((response.duration.value/60/60) < 30)  choise["bus"] += 2;
-            else if((response.duration.value/60/60) < 50)  choise["bus"] += 1;
+          this.googleService.getGoogleEstimatedTime(travelModes.bicycling, depart, destination).then( (response) => {
+            choise["velo"].time = response.duration.value;
+            choise["velo"].distance = response.distance.text;
+
+            if((response.duration.value/60/60) < 15)  choise["velo"].nb += 3;
+            else if((response.duration.value/60/60) < 30)  choise["velo"].nb += 2;
+            else if((response.duration.value/60/60) < 50)  choise["velo"].nb += 1;
           
             i++;  
-            if(i == 4)  resolve(choise);
+            if(i == 6)  resolve(choise);
           }).catch( (error) => {
             console.log(error);
           });
 
-          this.googleService.getGoogleEstimatedTime(travelModes.driving, "1600 Boulevard du Plateau-Saint-Joseph, Sherbrooke", "9 Rue du Cégep, Sherbrooke, QC J1E 2J4").then( (response) => {
-            if((response.duration.value/60/60) < 15)  choise["auto"] += 3;
-            else if((response.duration.value/60/60) < 30)  choise["auto"] += 2;
-            else if((response.duration.value/60/60) < 50)  choise["auto"] += 1;
+          this.googleService.getGoogleEstimatedTime(travelModes.driving, depart, destination).then( (response) => {
+            choise["auto"].time = response.duration.value;
+            choise["auto"].distance = response.distance.text;
+
+            if((response.duration.value/60/60) < 15)  choise["auto"].nb += 3;
+            else if((response.duration.value/60/60) < 30)  choise["auto"].nb += 2;
+            else if((response.duration.value/60/60) < 50)  choise["auto"].nb += 1;
           
             i++;  
-            if(i == 4)  resolve(choise);
+            if(i == 6)  resolve(choise);
           }).catch( (error) => {
             console.log(error);
           });
@@ -220,21 +237,8 @@ export class HomePage {
           this._ngZone.run(() => { });
       });
 
-    this.upcomingItems.unshift({
-        title: $event.srcElement.value,
-        description: "",
-        icon: 'walk', 
-        depart: 'Sherbrooke, Quebec',
-        destination: $event.srcElement.value,
-        address: $event.srcElement.value,
-        date: new Date().toUTCString(),
-        transport: 1
-    });
 
-    this.navCtrl.push(DetailTabsPage, {
-      item: this.upcomingItems[0]
-    });
-  }
+    }
   }
 
   setShameGauge() {
